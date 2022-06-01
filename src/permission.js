c991ab002d547a54faa5c783d2e88f29c672c9e0
lyj <1652038229@qq.com>
2022-06-01 16:28:56 +0800

    1.1

--- src/permission.js
+++ src/permission.js
@@ -1,21 +1,27 @@
 import router from "@/router";
 import store from "@/store";
-
-
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+Nprogress.configure({ showSpinner: false })
 router.beforeEach((to, from, next) => {
     const hasInfo = store.state.Login.userInfo
+    Nprogress.start()
     if (hasInfo) {
         if(to.path==='/login'){
             next({path:'/'})
+            Nprogress.done()
         }else {
             next()
+            Nprogress.done()
         }
 
     }else {
        if(to.path==='/login'){
            next()
+           Nprogress.done()
        }else {
            next({path:'/login'})
+           Nprogress.done()
        }
     }
 
